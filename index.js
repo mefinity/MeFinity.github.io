7ca6996bbaee695e548a1959ebdb5e9bb487b8ef
MeFinity <mefinity8@gmail.com>
2023-04-03 22:23:17 +0700

    it looks

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+function currenttime() {
+const date = new Date();
+
+const settime = document.getElementById("time");
+
+settime.textContent = date.toLocaleString('en-US', {
+  timeZone: 'Asia/Jakarta',
+  hour12: false,
+  hour: '2-digit',
+  minute: '2-digit'
+});
+}
+
+currenttime();
+});
